c3cb42d5caca39abfafbb326cea5cba2110d8d82
Yogesh jadhav <145186546+YogeshJadhav18@users.noreply.github.com>
2025-12-07 14:34:23 +0530

    rebuild Render

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: '/',
-});
+  plugins: [react()],
+  server: {
+    port: 5173, // Any port is fine
+  },
+  build: {
+    outDir: 'dist',
+  }
+})
